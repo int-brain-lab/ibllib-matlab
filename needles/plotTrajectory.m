e544187febcb4ec3029284874f50a48b9b03643d
Nick Steinmetz <nick.steinmetz@gmail.com>
2019-07-21 23:49:29 +0100

    fix minor inaccuracies in plotTrajectory, and make it plot at a certain aspect ratio

--- needles/plotTrajectory.m
+++ needles/plotTrajectory.m
@@ -5,8 +5,9 @@ function lab = plotTrajectory(ax, atlas, vectorStart, vectorEnd)
 
 % get points at 1µm spacing along the vector, including some beyond the
 % limits
-spacing = 1e-6; 
+spacing = 50e-6; 
 padding = 1e-3; % 1 mm on either side
+trajScale = 1e4*0.7;
 
 len = norm(vectorEnd-vectorStart);
 vectorDir = (vectorEnd-vectorStart)./len; 
@@ -18,24 +19,27 @@ allSamp = startSamp+trajX'*vectorDir;
 lab = labelsAlongVector(atlas, allSamp);
 
 % plot the areas
+trajX = trajX*trajScale; spacing = spacing*trajScale; padding = padding*trajScale;
 im = imagesc(1, trajX, lab, 'Parent', gca);
 hold on; 
-plot([0.5 1.5], padding*[1 1], 'k', 'LineWidth', 2.0); 
-plot([0.5 1.5], (max(trajX)-padding)*[1 1], 'k', 'LineWidth', 2.0); 
-fill([0.5 1.5 1.5 0.5], [0 0 padding padding], 'w', 'FaceAlpha', 0.7, 'EdgeAlpha', 0); 
-fill([0.5 1.5 1.5 0.5], max(trajX)-[0 0 padding padding], 'w', 'FaceAlpha', 0.7, 'EdgeAlpha', 0); 
+plot([0.5 1.5], padding*[1 1], 'k', 'LineWidth', 4.0); 
+plot([0.5 1.5], (max(trajX)-padding)*[1 1], 'k', 'LineWidth', 4.0); 
+fill([0.5 1.5 1.5 0.5], [0 0 padding padding], 'w', 'FaceAlpha', 0.8, 'EdgeAlpha', 0); 
+fill([0.5 1.5 1.5 0.5], max(trajX)-[0 0 padding padding], 'w', 'FaceAlpha', 0.8, 'EdgeAlpha', 0); 
 
 dLab = find(diff([-1; double(lab)])~=0);
-dLab(end+1) = numel(lab);
+if dLab(end)~=numel(lab); dLab(end+1) = numel(lab); end
 for u = 1:(numel(dLab)-1)
     uy(u) = mean(dLab(u:u+1));
-    uStr{u} = atlas.labels.acronym{lab(dLab(u))+1};
+    plot([0.5 1.5], trajX(dLab(u))*[1 1]-0.5*spacing, 'Color', [0.5 0.5 0.5], 'LineWidth', 0.5);
+    uStr{u} = atlas.labels.table.acronym{lab(dLab(u))+1};
 end
 
 [uy,ii] = sort(uy); 
 uStr = uStr(ii);
 
 % add labels
-set(ax, 'YTick', trajX(round(uy)), 'YTickLabel', uStr, 'XTick', [], 'TickDir', 'out'); 
+set(ax, 'YTick', trajX(round(uy)-1), 'YTickLabel', uStr, 'XTick', [], 'TickDir', 'out'); 
 box(ax, 'off'); 
+axis(ax, 'image');
 cmap = atlas.cmap; caxis([1 size(cmap,1)]); colormap(cmap);
